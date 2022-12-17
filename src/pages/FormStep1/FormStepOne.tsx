ffc63f0040772edd31a379da0b470514d3179e15
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-12-17 16:22:53 -0300

    feat: structure/styling in the first form step

--- src/pages/FormStep1/FormStepOne.tsx
+++ src/pages/FormStep1/FormStepOne.tsx
@@ -1,5 +1,57 @@
+import * as C from "./FormStepOneStyle";
+
+import { useNavigate } from "react-router-dom";
+import { useForm, FormActions } from "../../contexts/FormContext";
+
+import { Theme } from "../../components/Theme/Theme";
+import { ChangeEvent, useEffect } from "react";
+
 export const FormStepOne = () => {
+    const navigate = useNavigate();
+    const { state, dispatch } = useForm();
+
+    useEffect(() => {
+        dispatch({
+            type: FormActions.setCurrentStep,
+            payload: 1
+        });
+    }, []);
+
+    const handleNextStep = () => {
+        if (state.name !== "") {
+            navigate("/step2");
+        } else {
+            alert("Preencha os dados!");
+        }
+    }
+
+    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: FormActions.setName,
+            payload: e.target.value
+        });
+    }
+
     return (
-        <h1>FormStepOne</h1>
+        <Theme>
+            <C.Container>
+                <p>Passo 1/3</p>
+                <h1>Vamos começar com seu nome</h1>
+                <p>Preencha o campo abaixo com seu nome completo.</p>
+
+                <hr />
+                
+                <label>
+                    Seu nome completo
+                    <input
+                        type={"text"}
+                        autoFocus
+                        value={state.name}
+                        onChange={handleNameChange} />
+                </label>
+
+                <button onClick={handleNextStep}>Próximo</button>
+            </C.Container>
+        </Theme>
     );
 }
